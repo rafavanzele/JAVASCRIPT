98b8589da5af93a6fad7ca9536b3d16e04697d7d
rafavanzele <rafa_vanzele@hotmail.com>
2023-05-23 16:38:12 -0300

    exercícios de js + node

---
+++ aula11/ex008.js
@@ -0,0 +1,10 @@
+var vel = 40
+console.log(`A velocidade do seu carro é ${vel}km/h!`)
+
+if(vel > 60) {
+    console.log('Você está acima do limite de velocidade. MULTADO!')
+}
+
+console.log('Dirija sempre com cinto de segurança!')
+
+// Modelo de condição simples porque não tem ELSE (senão)
---
+++ aula11/ex009.js
@@ -0,0 +1,9 @@
+var país = 'Canadá'
+
+console.log(`Você está vivendo em ${país}`)
+
+if (país == 'Brasil') {
+    console.log('Parabéns, você é Brasileiro')
+} else {
+    console.log('Que pena, você é estrangeiro')
+}
