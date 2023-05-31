7d6a97468dfdbd5ef2ec125f59ff58aab1a56e73
rafavanzele <rafa_vanzele@hotmail.com>
2023-05-31 17:29:18 -0300

    exerc vetor/array

---
+++ aula15/ambiente.js
@@ -0,0 +1,13 @@
+let num = [5, 8, 2, 9, 3]
+
+num.sort() //colocando os elementos do vetor em ordem crescente
+
+console.log(num)
+
+//num[5] = 13 acrescentando o valor 13 no índice 5
+
+//num.push(24) acrescentando o valor 24 no próximo indice disponível
+
+console.log(`O vetor tem ${num.length} posições`)
+
+console.log(`Nosso vetor é o ${num}`)
